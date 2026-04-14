625709c96338caae06ce0b8efb7d4473263c7bca
Viviana <40200339@esmad.ipp.pt>
2026-04-14 20:20:24 +0100

    added base elements of hero section

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,5 +1,44 @@
 <template>
-  <section></section>
+  <section>
+    <div class="main-grid">
+      <h2 class="title-section">High-end residential design & build</h2>
+
+      <div class="cards">
+        <div class="card card-1">
+          <h3>10+</h3>
+          <p>years of experience</p>
+        </div>
+
+        <div class="card card-2">
+          <h3>75+</h3>
+          <p>completed projects</p>
+        </div>
+
+        <article class="card card-3">
+          <img src="../assets/images/carousel-house-1.jpeg" alt="modern house" />
+
+          <aside>
+            <header>
+              <!-- add logo -->
+            </header>
+
+            <span>
+              <h3>Ridge House</h3>
+              <p>
+                A modern home designed for comfortable living, featuring clean lines, open spaces,
+                and a strong connection to its surroundings
+              </p>
+            </span>
+
+            <footer>
+              <div class="arrow arrow-left"><-</div>
+              <div class="arrow arrow-right">-></div>
+            </footer>
+          </aside>
+        </article>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
