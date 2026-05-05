12ad8c718eb42cbd78d87cd47339da29220e0ead
Viviana <40200339@esmad.ipp.pt>
2026-05-05 12:40:35 +0100

    added about section and its main structure

--- src/LandingPage.vue
+++ src/LandingPage.vue
@@ -1,17 +1,34 @@
 <template>
   <div>
-    <HeroSection />
+    <div class="bg-image">
+      <HeroSection />
+      <AboutSection />
+    </div>
   </div>
 </template>
 
 <script>
 import HeroSection from "./components/HeroSection.vue";
+import AboutSection from "./components/AboutSection.vue";
 
 export default {
   components: {
     HeroSection,
+    AboutSection,
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.bg-image {
+  width: 100%;
+  height: 110vh;
+
+  background-image:
+    linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
+    url("./src/assets/images/house-background.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 50% 50%;
+}
+</style>
---
+++ src/components/AboutSection.vue
@@ -0,0 +1,64 @@
+<template>
+  <section>
+    <div class="about-title">
+      <h2>A modern studio with a clear focus</h2>
+
+      <span>
+        <p>
+          Aris House is a team working at the interaction of real estate, architecture, and
+          technology.
+        </p>
+        <button>Check projects</button>
+      </span>
+    </div>
+
+    <div class="grid-cards">
+      <article class="cards card-1">
+        <header>
+          <h3>01</h3>
+          <div class="logo-white-bg">X</div>
+        </header>
+
+        <h4>Focused expertise</h4>
+        <p>
+          We concentrate on a limited number of projects to ensure full attention, consistency and
+          control at every stage of the process
+        </p>
+      </article>
+
+      <article class="cards card-2">
+        <header>
+          <h3>02</h3>
+          <div class="logo-black-bg">X</div>
+        </header>
+
+        <h4>Thoughtful execution</h4>
+        <p>
+          Every element is carefully considered - from overall composition to the smallest
+          architectural and technical decision, with precision.
+        </p>
+      </article>
+
+      <article class="cards card-3">
+        <header>
+          <h3>03</h3>
+          <div class="logo-white-bg">X</div>
+        </header>
+
+        <h4>Long-term quality</h4>
+        <p>
+          Our work is guided by durability, relevance and carefully developed solutions that remain
+          strong, refined and appropriate over time.
+        </p>
+      </article>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+@import "@/assets/styles/AboutSection.css";
+</style>
